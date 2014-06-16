e19c3a385cb2ebe5982f0bdbe8bd15690d72a1f6
Nikhil Ranjan <nikhil.ranjan@paytm.com>
2014-06-16 18:04:30 +0530

    Example config file

---
+++ myconf.js
@@ -0,0 +1,7 @@
+// Request Test MID by emailing contactus@payzippy.com
+module.exports = {
+  merchant_id: 'MERCHANT_ID',
+  secret_key: 'MERCHANT_SECRET',
+  merchant_key_id: 'MERCHANT_KEY_ID',
+  callback_url: 'http://localhost:3000/pay',
+};
